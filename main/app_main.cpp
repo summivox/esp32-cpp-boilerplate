95a998010d370effe709b22cdaf52d5df989bae3
summivox <summivox@gmail.com>
2022-12-19 20:20:42 -0800

    Attempt to add absl::StrCat and friends to replace fat fmtlib

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -1,17 +1,47 @@
 #include <cstdio>
+#include <string>
 
-#include "example/hello.hpp"  // TODO: replace this with your own code
+#define USE_STR_CAT 0
+#define USE_STR_FORMAT 1
+#define USE_FMT 0
+
+#if USE_STR_CAT
+#include "absl/strings/str_cat.h"
+#endif  // USE_STR_CAT
+#if USE_STR_FORMAT
+#include "absl/strings/str_format.h"
+#endif  // USE_STR_FORMAT
+#if USE_FMT
+#include "fmt/core.h"
+#endif  // USE_FMT
+
+
 
 namespace {
 constexpr char TAG[] = "main";
 }
 
 extern "C" void app_main(void) {
-  // This is the entry point of user code.
-  // It is okay to return from this function.
+  std::string a =
+#if USE_STR_CAT
+      absl::StrCat("abc", 123, "def");
+#else
+      "";
+#endif
 
-  // TODO: replace this with your own code
+  std::string aa =
+#if USE_STR_FORMAT
+      absl::StrFormat("abc%09ddef", 123);
+#else
+      "";
+#endif
 
-  printf("\n\n\nHello World!\n\n\n");
-  example::StartExampleTask();
+  std::string b =
+#if USE_FMT
+      fmt::format("{}{}{}", "abc", 123, "def");
+#else
+      "";
+#endif
+
+  printf("%s\n%s\n%s\n", a.c_str(), aa.c_str(), b.c_str());
 }
